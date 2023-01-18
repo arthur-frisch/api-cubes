62e29d3f8c08ff994b6165f0c56727fdbb581307
Arthur <arthur060704@gmail.com>
2023-01-18 16:07:46 +0100

    Add linter on save

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,25 @@
-import {Response, Request} from 'express';
+import { Response, Request } from 'express';
 import express from 'express';
 
 const app = express();
 const port = 3000;
 
 type reqQuery = {
-    query: {
-        nb: number
-    }
+  query: {
+    nb: number;
+  };
 };
 
+const test = () => {
+  console.log('test');
+};
 
-app.get('/', async (req : reqQuery, res : Response) =>  {
+app.get('/', async (req: reqQuery, res: Response) => {
   const parameter = req.query.nb;
   res.send(parameter);
-  console.log("test");
+  console.log('test');
 });
+
 app.listen(port, () => {
   return console.log(`server is listening on ${port}`);
 });
