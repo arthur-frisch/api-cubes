00ab6d31e01e01f035d5c472729a2c85ed84bef0
Arthur <arthur060704@gmail.com>
2023-01-19 13:06:20 +0100

    correct dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,10 @@
 import { Response } from 'express';
 import express from 'express';
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 const app = express();
-const port = 3000;
-
 type reqQuery = {
   query: {
     nb: number;
@@ -13,11 +12,11 @@ type reqQuery = {
 };
 
 app.get('/', async (req: reqQuery, res: Response) => {
+  bcrypt.hash('testtest', 10).then(hash => console.log(hash));
   const parameter = req.query.nb;
   res.send(parameter);
-  console.log('test');
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   return console.log(`server is listening on port : ${process.env.PORT}`);
 });
