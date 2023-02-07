0e3bdc15d35706e00fd75258884e3e9c69d44b74
Arthur <arthur060704@gmail.com>
2023-02-07 11:50:02 +0100

    feat(bdd):add connection with sql server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import { Response } from 'express';
 import express from 'express';
 import dotenv from 'dotenv';
+import sql from 'mssql/msnodesqlv8';
 
 dotenv.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const config = {
+  server: 'localhost\\sqlexpress',
+  database: 'cesi',
+  driver: 'msnodesqlv8',
+  options: {
+    trustedConnection: true,
+  },
+};
 
 type reqQuery = {
   query: {
@@ -13,10 +22,24 @@ type reqQuery = {
 };
 
 app.get('/', async (req: reqQuery, res: Response) => {
+  // console.log(sql);
+  sql.connect(config, function (err) {
+    if (err) {
+      console.log(err);
+    }
+    const request = new sql.Request();
+    request.query('select * from [Logs]', (err, records) => {
+      if (err) console.log(err);
+      else console.log(records);
+    });
+  });
+  // const result =
+  //   await sql.query`select * from Logs where idLog = ${req.query.nb}`;
+  // console.log(result);
   const parameter = req.query.nb;
   res.send(parameter);
 });
 
 app.listen(port, () => {
-  return console.log(`server is listening on port : ${process.env.PORT}`);
+  return console.log(`server is listening on port : ${port}`);
 });
