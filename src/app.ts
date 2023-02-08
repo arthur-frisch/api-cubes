20905f7ca935d279d93f3785773223006d32ddaa
arthur-frisch <arthur060704@gmail.com>
2023-02-08 01:29:54 +0100

    feat(mssql): uninstall package

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,10 @@
 import { Response } from 'express';
 import express from 'express';
 import dotenv from 'dotenv';
-import sql from 'mssql/msnodesqlv8';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
-const config = {
-  server: process.env.SERVER ?? '',
-  database: process.env.DATABASE ?? '',
-  driver: process.env.DRIVER ?? '',
-  options: {
-    trustedConnection: true,
-  },
-};
 type reqQuery = {
   query: {
     nb: number;
@@ -21,16 +12,6 @@ type reqQuery = {
 };
 
 app.get('/', async (req: reqQuery, res: Response) => {
-  sql.connect(config, function (err) {
-    if (err) {
-      console.log(err);
-    }
-    const request = new sql.Request();
-    request.query('select * from [Logs]', (err, records) => {
-      if (err) console.log(err);
-      else console.log(records);
-    });
-  });
   const parameter = req.query.nb;
   res.send(parameter);
 });
