34abee2dd396470cf46c776a3e1ef233ed11548a
arthur-frisch <arthur060704@gmail.com>
2023-03-05 19:24:15 +0100

    Add docker-compose and prisma

---
+++ prisma/migrations/20230305181809_first_migration/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `firstname` VARCHAR(50) NOT NULL,
+    `lastname` VARCHAR(50) NOT NULL,
+    `test` VARCHAR(50) NOT NULL,
+    `createdAt` TIMESTAMP(0) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
